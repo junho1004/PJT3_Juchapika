482cd98171b353aea375276bbc8fb684b72e6204
wonseok <bulnabang99@gmail.com>
2023-04-13 15:41:39 +0900

    Fix: mainClass Name 수정

--- backend/security/src/main/java/com/svs/Supervision/SupervisionApplication.java
+++ backend/security/src/main/java/com/svs/Supervision/SupervisionApplication.java
@@ -1,13 +1,13 @@
-package com.svs.security;
+package com.svs.Supervision;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SupervisionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
+		SpringApplication.run(SupervisionApplication.class, args);
 	}
 
 }
--- backend/security/src/test/java/com/svs/Supervision/SecurityApplicationTests.java
+++ backend/security/src/test/java/com/svs/Supervision/SecurityApplicationTests.java
@@ -1,4 +1,4 @@
-package com.svs.security;
+package com.svs.Supervision;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
