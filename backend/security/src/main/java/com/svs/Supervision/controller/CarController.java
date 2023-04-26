2c88e5ba8886211de989fba3ab0bc9feff8eda58
wonseok <bulnabang99@gmail.com>
2023-04-26 16:01:43 +0900

    Feat: Car 기능 관련 DTO 생성 및 controller mapping 생성

--- backend/security/src/main/java/com/svs/Supervision/controller/CarController.java
+++ backend/security/src/main/java/com/svs/Supervision/controller/CarController.java
@@ -1,13 +1,36 @@
 package com.svs.Supervision.controller;
 
+import com.svs.Supervision.dto.request.CarNumberRequestDto;
+import com.svs.Supervision.entity.user.Admin;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import static io.swagger.v3.oas.integration.StringOpenApiConfigurationLoader.LOGGER;
 
 @Tag(name = "Car", description = "차량 관련 API")
 @RestController
 @RequestMapping("/car")
 @RequiredArgsConstructor
 public class CarController {
+
+    @PostMapping("/number")
+    @Operation(summary = "차량 번호판 등록", description = "단속된 차량 번호판을 저장합니다.")
+    public ResponseEntity<?> addCarNumber(@RequestBody CarNumberRequestDto carNumberRequestDto,
+                                          @Parameter(hidden = true)
+                                          @AuthenticationPrincipal Admin admin) {
+        LOGGER.info("addCarNumber() 호출 : " + carNumberRequestDto);
+
+    }
+
+
+    @GetMapping("/number")
+    @Operation(summary = "차량 번호판 조회", description = "단속된 차량 번호판을 조회합니다.")
+    public ResponseEntity<?> listCarNumber() {
+
+    }
 }
