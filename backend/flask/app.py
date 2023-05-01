bdaf1f2a7c03f72d6cd071aa87b6de81d3ac562d
young <gobuig00@naver.com>
2023-05-01 17:39:31 +0900

    feat : add flask Server

---
+++ backend/flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import dbModule
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    db_class = dbModule.Database()
+    return jsonify(db_class.executeAll("select * from car"))
+
+
+@app.route('/aa')
+def aa():
+    return 'aa'
+
+
+if __name__ == '__main__':
+    app.run()
+
+application = app
