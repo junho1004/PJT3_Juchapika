eaa6141f72405ddd1de0d83a64853598aa57afd9
young <gobuig00@naver.com>
2023-05-10 10:56:21 +0900

    feat:live Streaming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,15 @@ import VideoStorage from './pages/VideoStorage/VideoStorage'
 import Login from './pages/FirstPage/Login';
 
 //네비
-import Viewer from './useViewer';
+// import Viewer from './useViewer';
 
 //범법자용 고지서
 import Feeletter from "./pages/ForPerson/FeeLetter";
 import PayHistory from "./pages/ForPerson/PayHistory"
 
+// Kinesis video stream
+import KinesisVideoViewer from "./pages/LiveStream/KinesisVideoViewer";
+import TestLive from "./pages/LiveStream/testLive";
 function App() {
   return (
     <div className="App">
@@ -22,10 +25,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/main" element={<Main />} />
-          <Route exact path="/live" element={<Viewer />} />
-          <Route exact path="/videostorage" element={<VideoStorage/>}/>
-          <Route exact path="/feeletter" element={<Feeletter/>}/>
-          <Route exact path="/payhistory" element={<PayHistory/>}/>
+          {/* <Route exact path="/live" element={<Viewer />} /> */}
+          <Route exact path="/videostorage" element={<VideoStorage />} />
+          <Route exact path="/feeletter" element={<Feeletter />} />
+          <Route exact path="/payhistory" element={<PayHistory />} />
+          <Route exact path="/live" element={<KinesisVideoViewer />} />
+          <Route exact path="/live1" element={<TestLive />} />
         </Routes>
       </BrowserRouter>
     </div>
