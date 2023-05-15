c9860779580af41e7b643d5ea81c6d6124e84a11
wonseok <bulnabang99@gmail.com>
2023-05-15 15:19:21 +0900

    Etc: webSocket

---
+++ backend/webSocket/webSocketServer.py
@@ -0,0 +1,38 @@
+import asyncio
+import websockets
+
+# Define the global variable
+react_server = None
+
+
+async def handle_message(websocket, path):
+    async for message in websocket:
+        print(f"Received message: {message}")
+        # Send message to React server
+        react_server.send(message)
+
+
+async def start_websocket_server():
+    async with websockets.serve(handle_message, "52.79.199.205", 8082):
+        await asyncio.Future()  # Run forever
+
+
+# Define the React server address and port
+REACT_SERVER_ADDRESS = "52.79.199.205"
+REACT_SERVER_PORT = 80
+
+
+# Create a WebSocket connection to the React server
+async def connect_to_react_server():
+    global react_server
+    async with websockets.connect(f"ws://{REACT_SERVER_ADDRESS}:{REACT_SERVER_PORT}") as websocket:
+        react_server = websocket
+        print("Connected to React server")
+
+
+# Start the WebSocket server and connect to the React server
+async def main():
+    await asyncio.gather(start_websocket_server(), connect_to_react_server())
+
+
+asyncio.run(main())
