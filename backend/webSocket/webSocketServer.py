541c45646b098c5318fb406afd9211677bee2880
bulnabang99 <bulnabang99@gmail.com>
2023-05-15 21:47:15 +0900

    Etc:

--- backend/webSocket/webSocketServer.py
+++ backend/webSocket/webSocketServer.py
@@ -25,11 +25,13 @@ WEBSOCKET_SERVER_PORT = 8082
 # Create a WebSocket connection to the WebSocket server
 async def connect_to_websocket_server():
     global react_server
+    
+    await asyncio.sleep(1)  # Wait for the WebSocket server to start
     async with websockets.connect(f"wss://{WEBSOCKET_SERVER_ADDRESS}:{WEBSOCKET_SERVER_PORT}") as websocket:
         react_server = websocket
         print("Connected to WebSocket server")
 
-#
+
 # Start the WebSocket server and connect to the WebSocket server
 async def main():
     await asyncio.gather(start_websocket_server(), connect_to_websocket_server())
