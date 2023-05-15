532ea1a0db13cf5e1b63d484f1d2ca40b9abdf34
wonseok <bulnabang99@gmail.com>
2023-05-15 16:20:17 +0900

    Etc:

--- backend/webSocket/webSocketServer.py
+++ backend/webSocket/webSocketServer.py
@@ -1,5 +1,6 @@
 import asyncio
 import websockets
+import ssl
 
 # Define the global variable
 react_server = None
@@ -13,7 +14,10 @@ async def handle_message(websocket, path):
 
 
 async def start_websocket_server():
-    async with websockets.serve(handle_message, "0.0.0.0", 8082):
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain("/etc/nginx/certs/fullchain.pem", "/etc/nginx/certs/privkey.pem")
+
+    async with websockets.serve(handle_message, "0.0.0.0", 443, ssl=ssl_context):
         await asyncio.Future()  # Run forever
 
 
@@ -25,7 +29,11 @@ REACT_SERVER_PORT = 443
 # Create a WebSocket connection to the React server
 async def connect_to_react_server():
     global react_server
-    async with websockets.connect(f"ws://{REACT_SERVER_ADDRESS}:{REACT_SERVER_PORT}") as websocket:
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
+    async with websockets.connect(f"wss://{REACT_SERVER_ADDRESS}:{REACT_SERVER_PORT}", ssl=ssl_context) as websocket:
         react_server = websocket
         print("Connected to React server")
 
