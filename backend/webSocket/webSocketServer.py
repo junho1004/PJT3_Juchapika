abecbfb5d8568f440affbdf851f53505899ab1e1
wonseok <bulnabang99@gmail.com>
2023-05-16 11:14:17 +0900

    Etc:

--- backend/webSocket/webSocketServer.py
+++ backend/webSocket/webSocketServer.py
@@ -3,12 +3,12 @@ import websockets
 import ssl
 import pathlib
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(
-    pathlib.Path('/path/to/container/fullchain.pem'),
-    pathlib.Path('/path/to/container/privkey.pem')
-)
-
+# ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# ssl_context.load_cert_chain(
+#     pathlib.Path('/path/to/container/fullchain.pem'),
+#     pathlib.Path('/path/to/container/privkey.pem')
+# )
+# , ssl=ssl_context
 
 async def handle_message(websocket, path):
     async for message in websocket:
@@ -18,7 +18,7 @@ async def handle_message(websocket, path):
 
 
 async def start_websocket_server():
-    async with websockets.serve(handle_message, "0.0.0.0", 8082, ssl=ssl_context):
+    async with websockets.serve(handle_message, "0.0.0.0", 8082):
         print("WebSocket server started")
         await asyncio.Future()  # Run forever
 
