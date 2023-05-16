3a13d81d29b93e0f35db30885a9d8f87a593f637
wonseok <bulnabang99@gmail.com>
2023-05-16 12:03:32 +0900

    Feat:

--- backend/webSocket/webSocketServer.py
+++ backend/webSocket/webSocketServer.py
@@ -35,12 +35,12 @@ import websockets
 import ssl
 import pathlib
 
+
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ssl_context.load_cert_chain(
     pathlib.Path('/path/to/container/fullchain.pem'),
     pathlib.Path('/path/to/container/privkey.pem')
 )
-
 
 async def handle_message(websocket, path):
     async for message in websocket:
@@ -49,41 +49,33 @@ async def handle_message(websocket, path):
         # ...
 
 
-async def process_request(path, request_headers):
-    # Allow requests from the specified origin
-    origin = request_headers.get("Origin", "")
-    if origin != "https://juchapika.site":
-        return
-
-    # Allow the Upgrade header for WebSocket handshake
-    if "Upgrade" not in request_headers:
-        return
-
-    # Add the necessary CORS headers
-    headers = [
-        ("Access-Control-Allow-Origin", "https://juchapika.site"),
-        ("Access-Control-Allow-Headers", "Upgrade, Origin, Content-Type, Accept"),
-        ("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
-        ("Access-Control-Allow-Credentials", "true"),
-    ]
-
-    return headers
-
-
 async def start_websocket_server():
-    async with websockets.serve(
-            handle_message,
-            "0.0.0.0",
-            8082,
-            ssl=ssl_context,
-            process_request=process_request,
-    ):
+    async with websockets.serve(handle_message, "0.0.0.0", 8082, ssl=ssl_context):
         print("WebSocket server started")
         await asyncio.Future()  # Run forever
 
 
 async def main():
-    await start_websocket_server()
+    server = await start_websocket_server()
+
+    # Enable CORS
+    async def set_cors_headers(request):
+        request_headers = request.headers
+        origin = request_headers.get("Origin", "")
+        if origin == "https://juchapika.site":
+            response_headers = [
+                ("Access-Control-Allow-Origin", origin),
+                ("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
+                ("Access-Control-Allow-Headers", "Content-Type"),
+                ("Access-Control-Max-Age", "3600"),
+            ]
+            return response_headers
+
+    server.start_server = websockets.serve(
+        server._run, server.host, server.port, klass=server.klass, create_protocol=server.create_protocol, process_request=set_cors_headers
+    )
+    await server.wait_closed()
 
 
 asyncio.run(main())
+
