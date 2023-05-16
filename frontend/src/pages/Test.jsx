12c9e2f45f92bcc1617e79e842e0cd7b1ebcee30
wonseok <bulnabang99@gmail.com>
2023-05-16 09:32:01 +0900

    Feat: React 서버 웹 소켓 패킷 수신

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -9,20 +9,21 @@ export default function Test() {
 
   useEffect(() => {
     socket.on('message', (data) => {
-      const { value1, value2 } = JSON.parse(data);
-      setValue1(value1);
-      setValue2(value2);
+      const { battery, time } = JSON.parse(data); // 메시지 파싱
+      setValue1(battery); // value1 상태 변수에 battery 값을 할당
+      setValue2(time); // value2 상태 변수에 time 값을 할당
     });
     return () => {
-      socket.off('message'); // Clean up the event listener when component unmounts
+      socket.off('message'); // 컴포넌트 언마운트 시 이벤트 리스너 정리
     };
   }, []);
+  
 
   return (
     <div>
       <h1>WebSocket Receiver Example</h1>
-      <p>Value 1: {value1}</p>
-      <p>Value 2: {value2}</p>
+      <p>- battery: {value1}</p>
+      <p>- time: {value2}</p>
     </div>
   );
 }
