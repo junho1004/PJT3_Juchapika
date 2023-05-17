3bf0046cff36e05299f8b3f0aa54ea0d0bfa6224
young <gobuig00@naver.com>
2023-05-17 15:28:27 +0900

    feat : fee api

--- backend/security/src/main/java/com/svs/Supervision/controller/FeeController.java
+++ backend/security/src/main/java/com/svs/Supervision/controller/FeeController.java
@@ -1,18 +1,27 @@
 package com.svs.Supervision.controller;
 
+import com.svs.Supervision.dto.request.record.RecordCarNumRequestDto;
 import com.svs.Supervision.dto.response.api.ApiResponseDto;
 import com.svs.Supervision.dto.response.record.RecordCarNumResponseDto;
+import com.svs.Supervision.entity.user.User;
 import com.svs.Supervision.service.fee.FeeService;
 import com.svs.Supervision.service.record.RecordService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.net.URLEncoder;
+import java.util.List;
+
+import static io.swagger.v3.oas.integration.StringOpenApiConfigurationLoader.LOGGER;
+
 @Tag(name = "Fee", description = "과태료 고지서 API")
 @RestController
 @RequestMapping("/feeletter")
@@ -47,5 +56,19 @@ public class FeeController {
         System.out.println(recordCarNumResponseDto.getCarNum());
         return new ResponseEntity(new ApiResponseDto(true, "searchRecordById successfully@", recordCarNumResponseDto),HttpStatus.OK);
     }
+    @PostMapping("/carnum")
+    @Operation(summary = "단속 차량 조회", description = "번호판 기준으로 단속된 차량의 단속 기록들을 조회합니다.")
+    public ResponseEntity<?> searchRecordByCarNum(@RequestBody RecordCarNumRequestDto recordCarNumRequestDto,
+                                                  @Parameter(hidden = true) @AuthenticationPrincipal User user) {
+        LOGGER.info("searchRecord() 호출 : " + recordCarNumRequestDto);
+        List<RecordCarNumResponseDto> recordCarNumResponseDtoList = recordService
+                .searchRecord(recordCarNumRequestDto.getCarNum());
 
+        if (recordCarNumResponseDtoList == null) {
+            return new ResponseEntity(new ApiResponseDto(false, "searchRecord Fail@", null), HttpStatus.OK);
+        } else {
+            return new ResponseEntity(
+                    new ApiResponseDto(true, "searchRecord successfully@", recordCarNumResponseDtoList), HttpStatus.OK);
+        }
+    }
 }
